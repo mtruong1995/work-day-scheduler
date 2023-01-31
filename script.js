4f27df779f97c46af8f3f544445174b12a1d4f58
may <mtruong@hotmail.co.uk>
2023-01-31 00:32:14 +0000

    insert code for box and button

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 var todayDate = moment().format("D MMM YYYY");
 $("#currentDay").text(todayDate);
+
+$(document).ready(function(){
+
+    $(".saveBtn").click(function(){
+        alert("Timetable saved");
+
+        let timeDay = $(this).parent().attr("id")
+        let task = $(this).siblings(".input-group").val();
+
+        console.log(timeDay);
+        console.log(task);
+
+        localStorage.setItem(timeDay, task);
+
+        $(".notification").addClass
+    })
+    
+    function hourUpdate(){
+        const currentTime = moment().hours();
+
+        $(".time-block").each(function(){
+            let blockHour = parseInt($(this).attr("id").split("-")[1]);
+            alert(blockHour);
+        })
+
+        console.log($(".time-block"))
+    }
+
+})
